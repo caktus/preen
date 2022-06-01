8e1f5525a19488c1ee6a49cfec90fcedf5967dd6
Jeremy Gibson <jgibson@caktusgroup.com>
2022-06-01 11:54:02 -0400

    Current changes

--- preen/block.py
+++ preen/block.py
@@ -1,2 +1,20 @@
-def i_am_a_real_package():
-    print("I am an editable real package")
+from wagtail.core import blocks
+
+
+class Analyzer:
+
+    def __init__(self, block) -> None:
+        self.block = block
+        self.declared_blocks = []
+        self.base_blocks = []
+        self.block_representation = {}
+
+    def analyze_block(self):
+        for dblock in self.declared_blocks:
+            if isinstance(dblock, blocks.StreamBlock):
+                self.declared_blocks.append(Analyzer(dblock))
+            else:
+                breakpoint()
+                self.block_representation[dblock]
+        for bblock in self.base_blocks:
+            self.base_blocks.append(Analyzer)
