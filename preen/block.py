4aa1d2078527124d0db28af4c988a442fe77d670
Jeremy Gibson <jgibson@caktusgroup.com>
2022-06-02 09:10:42 -0400

    Working representation for complex block types

--- preen/block.py
+++ preen/block.py
@@ -14,15 +14,24 @@ class Analyzer:
     def __init__(self, block) -> None:
         self.block = block
         self.block_representation = {}
+        self.list_blocks = {}
+        self.stream_blocks = {}
+        self.other_complex = {}
 
     def to_representation(self):
         for name, block in self._get_iter_blocks().items():
             if block.__class__.__name__ == 'ListBlock':
                 self.block_representation[name] = self.list_block_render(block)
+                self.list_blocks[name] = block
             if block.__class__.__name__ == 'StreamBlock':
-                self.block_representation[name] = [self.stream_block_render(block)]
+                sub_blocks = []
+                for child_name, child_block in block.child_blocks.items():
+                    sub_blocks.append(self.stream_block_render(child_name, child_block))
+                self.block_representation[name] = sub_blocks
+                self.stream_blocks[name] = block
             elif block.__class__.__name__ not in block_classes:
                 self.block_representation[name] = self.block_render(block)
+                self.other_complex[name] = block
             else:
                 self.block_representation[name] = "<insert-value>"
 
@@ -37,15 +46,12 @@ class Analyzer:
         analyzer.to_representation()
         return analyzer.block_representation
 
-    def stream_block_render(self, block):
+    def stream_block_render(self, block_name, block):
         stream_parent = {}
-        items = {}
-        for k, v in block.child_blocks.items():
-            analyzer = Analyzer(v)
-            analyzer.to_representation()
-            items[k] = analyzer.block_representation
-            stream_parent["type"] = k
-            stream_parent["value"] = items
+        analyzer = Analyzer(block)
+        analyzer.to_representation()
+        stream_parent["type"] = block_name
+        stream_parent["value"] = analyzer.block_representation
         return stream_parent
 
     def block_render(self, block):
